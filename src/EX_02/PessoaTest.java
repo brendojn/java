0c7a8c34e264e7d9c7db1ea65d2adac32aa5ef4b
brendojn <brendojnio@gmail.com>
2018-08-18 13:55:47 -0300

    Adicionando teste

---
+++ src/EX_02/PessoaTest.java
@@ -0,0 +1,22 @@
+package EX_02;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PessoaTest {
+    Pessoa pessoa;
+
+    @Before
+    public void setup() throws Exception{
+        Pessoa pessoa = new Pessoa();
+    }
+
+
+    @Test
+    public void testMaiorIdade() {
+
+    }
+
+}
